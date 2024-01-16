fb109777a27d264035583de83100d16429f865b9
Denis Zholob <dddgamer3x@gmail.com>
2024-01-16 14:10:00 -0800

    Add multi platform build commands
    Update package versions and add more build commands

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       './**/*.html',
     ]
   },
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
     // fill: theme => ({
