c0306b5a2124aa6c1511b0b759da0fb11e84f3fa
Denis Zholob <dddgamer3x@gmail.com>
2025-02-28 16:11:09 -0700

    Remove sandbox spec and styles, update with lifecycle hooks and own blank service

--- libs/pages/src/sandbox-page/sandbox-page.component.ts
+++ libs/pages/src/sandbox-page/sandbox-page.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SandboxPageService } from './sandbox-page.service';
 
 @Component({
   selector: 'pwm-sandbox-page',
   templateUrl: './sandbox-page.component.html',
-  // styleUrls: ['./sandbox-page.component.scss'],
 })
-export class SandboxPageComponent {}
+export class SandboxPageComponent implements OnInit, OnDestroy {
+  // #region Class Properties
+  private readonly clearSub$ = new Subject<void>();
+  // #endregion
+
+  // #region Constructor + Lifecycle
+  constructor(private sandboxPageService: SandboxPageService) {}
+
+  public ngOnInit(): void {}
+
+  public ngOnDestroy(): void {
+    this.clearSub$.next();
+    this.clearSub$.complete();
+  }
+  // #endregion
+
+  // #region Template Methods
+
+  // #endregion
+
+  // #region Helper Methods
+
+  // #endregion
+}
